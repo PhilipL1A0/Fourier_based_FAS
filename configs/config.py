5f18e331da32ae7191ef8700c73de59e4e16b2ad
Philip <lzhifei3117@163.com>
2025-03-25 14:31:12 +0800

    更新配置文件，添加Focal Loss损失函数，启用数据增强

--- configs/config.py
+++ configs/config.py
@@ -1,5 +1,8 @@
 # config.py
 
+from sympy import use
+
+
 class Config:
     # 训练基础配置
     backbone = "resnet"          # 模型主干
@@ -10,6 +13,7 @@ class Config:
     num_classes = 2              # 二分类任务
     input_channels = 1           # 频域数据通道数（假设是单通道，如幅度谱）
     attention_type = "cbam"
+    loss_func = "focal_loss"  # 损失函数（交叉熵）,可选择focal_loss
 
     # 训练参数配置
     batch_size = 32
@@ -19,7 +23,6 @@ class Config:
     num_workers = 4
 
     # 数据路径配置
-    model_name = f"{backbone}_{epochs}_{batch_size}_{lr}_{epochs}.pth"
     data_dir = "/media/main/lzf/FBFAS/data/dataset"
     output_dir = "/media/main/lzf/FBFAS/outputs"
 
@@ -29,17 +32,19 @@ class Config:
     use_amp = True             # 混合精度
     use_warmup = True          # Warmup学习率
     use_lr_cos = True          # 余弦退火学习率
+    use_augmentation = True    # 数据增强
 
     # 训练策略参数
     patience = 10         # 早停等待次数
     devices = [0, 1]      # 多GPU设备
     amp_opt_level = "O1"  # 混合精度级别（如O1/O2）
     warmup_epochs = 5     # Warmup步数
-    total_steps = 1000    # 总训练步数（用于Warmup+Cosine）
-    lr_min = 1e-5         # 最小学习率
+    lr_min = 1e-6         # 最小学习率
     lr_cycles = 1         # 余弦周期数
 
     # 其他可选功能（可扩展）
     use_gradient_clipping = True  # 梯度裁剪
     clip_value = 1.0              # 裁剪值
     use_tensorboard = True        # Tensorboard可视化
+
+    model_name = f"{backbone}_L2_{epochs}_{batch_size}_{lr}_{epochs}"
